f591a8b17dd18eef3a9d5f30cc71147ce5f4125a
cathydutton <info@cathydutton.co.uk>
2018-04-24 19:16:06 +0100

    Add Mongodb conection

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,40 +1,44 @@
 var bodyParser = require('body-parser');
+var mongoose =  require('mongoose');
 
 // Connect to database
-// https://www.thenetninja.co.uk/courses/node-js-tutorial-for-beginners
-// var mongoose =  require('mongoose');
-// mongoose.connect('mongodb://test:test@ds155699.mlab.com:55699/todo');
+mongoose.connect('mongodb://test:test@ds155699.mlab.com:55699/todo');
+// Database link https://mlab.com/databases/todo
 
-// // Create a schema - this is like a blueprint
-// var todoSchema = new mongoose.Schema({
-//   item: String
-// });
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String
+});
 
-// var Todo = mongoose.model('Todo', todoSchema)
-// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-//   if (err) throw err;
-//   console.log("item saved");
-// });
+var Todo = mongoose.model('Todo', todoSchema)
+
 
 module.exports = function(app) {
 
-var data = [ {item: "Get milk"}, {item: "walk cat"}, {item: "clean fridge"}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.get('/todo', function (req, res) {
-  res.render("todo", {todos: data});
+  // Get data from mongodb and pass it to the view
+  Todo.find({}, function(err, data){
+    if (err) throw err;
+    res.render("todo", {todos: data});
+  }); 
 });
 
 app.post('/todo', urlencodedParser,  function (req, res) {
-  data.push(req.body)
-  res.json(data);
+  // Get data from the view and pass it to mongodb
+  var newTodo = Todo(req.body).save(function(err,data){
+    if (err) throw err;
+    res.json(data);
+  })
 });
 
 app.delete('/todo/:item', function (req, res) {
-  data = data.filter(function(todo) {
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+  //Delete the requested data from mongodb
+  Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+    if (err) throw err;
+    res.json(data);
   });
-  res.json(data);
 });
 
-};
+}
